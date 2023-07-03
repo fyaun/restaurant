de4d0ce32921ab7dc75c48a29db405c63683fc0d
fyaun <74760110+fyaun@users.noreply.github.com>
2023-07-03 14:09:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 $(function() {
 
+
+
+
+  //ローディング画面の表示
+$(window).on('load',function(){
+  $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#loading_box").delay(1200).fadeOut('slow');//ローディングテキストを1.2秒（1200ms）待機してからフェードアウト
+});
+
+
     // ハンバーガー
     $('.drawer-icon').on('click', function(e) {
         e.preventDefault();
